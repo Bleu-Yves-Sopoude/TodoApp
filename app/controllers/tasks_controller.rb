ce3d2f83ecfefb90dd59c3eae95311bb2bef96f8
Bleu-Yves-Sopoude <bleuyves@gmail.com>
2025-02-17 06:31:39 +0000

    add model and controller

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,44 @@
+class TasksController < ApplicationController
+    before_action :set_task, only: [:show,:edit, :update, :delete, :index]
+
+    def index
+        @tasks = Task.all
+    end
+
+    def new
+        @task =Task.new
+    end
+
+    def create
+        @task =Task.new(task_params)
+        if @task.save
+            redirect_to @task, notice:"Task succesfuly created"
+        else
+            render :new
+        end
+    end
+
+    def edit
+    end
+
+    def update
+        if @task.update(task_params)
+            redirect_to @task, notice:"Task was succesfully updated"
+        else
+            render :edit
+        end
+    end
+
+    private
+
+    def task_params
+        params.require(:task).permit(:name, :description)
+    end
+
+    def set_task
+        @task = Task.find(params[:id])
+    end
+
+
+
+end
