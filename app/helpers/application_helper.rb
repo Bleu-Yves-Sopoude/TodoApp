b256d96e34953d3074d83ec9c027b0666e0c6e3c
Bleu-Yves-Sopoude <bleuyves@gmail.com>
2025-04-23 05:50:19 +0000

    improve ui

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def icon_for_status(status)
+        case status
+        when 'pending'     then "🕒"
+        when 'in_progress' then "🚧"
+        when 'completed'   then "✅"
+        else "❓"
+        end
+      end
 end
